3b0b77c7873fc6d59f03c8a9da81c76541d0b6eb
Rakib Khan <rakibkhan@live.co.uk>
2019-02-14 18:04:00 +0000

    added more files

---
+++ problem_template.js
@@ -0,0 +1,11 @@
+let ans1 = document.querySelector('.answer1');
+// console.log(ans1);
+
+function showAns1(){
+    if (ans1.style.display === "none"){
+    ans1.style.display = "block";
+    }
+    else{
+        ans1.style.display = "none";
+    }
+}
